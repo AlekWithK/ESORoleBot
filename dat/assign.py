7caf029cd71b3a5feda8a1e4e58cdc6a9935eab8
AlekWithK <38448418+AlekWithK@users.noreply.github.com>
2023-05-03 20:06:51 -0700

    Major bugfix on role assignment
    was not assigning partial HM's correctly, now does

--- dat/assign.py
+++ dat/assign.py
@@ -1,43 +1,30 @@
 import discord
-from dat.clear_dat import heiarchy
+from dat.clear_dat import value_dict, trial_tags
 from dat.strings import *
 from dat.embeds import error_embed
 
 # clears = ['vss', 'vrg_tri', etc...]
 async def assign(coll, clears, guild, channel, author):    
     num_assign = 0
+    
     try:
-        clears_list = []
+        clears_dict = {}
         result = coll.find_one({"_id": guild.id}, {"emoji": 0})
         
         # Create a dictionary with only clears that the user has that are also present in 
-        # the server specific database
+        # the server specific database. This automatically updates any lower order clear to 
+        # the higher order version due to the order of something im not sure honestly
         for clear in clears:
             if result.get(clear):
-                clears_list.append((clear, result[clear]))
-        
-        # Assign roles if not already held, and create a list of assigned roles           
-        assigned_roles = []
-        clears_sorted = sorted(clears_list, key=lambda x: [tup[1] for tup in heiarchy if tup[0] == x[0]] or [float('inf')])[::-1]
-        #print(clears_sorted)        
+                clears_dict.update({clear[:3] : ((result[clear]), value_dict[clear])})
                 
-        bypass = []
-        for clear, role in result.items():
-            names = [role.name for role in author.roles]
-            if role in names:
-                bypass.append(clear[:3])
-        
-        for clear in clears_sorted:
-            role = discord.utils.get(guild.roles, name=clear[1])
-            tag = clear[0][:3]
-            if role not in author.roles and tag not in bypass:
+        assigned_roles = []  
+        for tag, info in clears_dict.items():
+            role = discord.utils.get(guild.roles, name=info[0])
+            if role not in author.roles:
                 await author.add_roles(role)
-                assigned_roles.append(clear[1])
-                num_assign += 1
-                
-                if clear[0] != 'vas_felms' and clear[0] != 'vss_ice':
-                    bypass.append(tag)
-                
+                assigned_roles.append(info[0])
+                num_assign += 1           
         
         return assigned_roles, num_assign
     
